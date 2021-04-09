3b89461197c25c87ad838ee9802c6d210124221e
supapong <kiraz_coe@hotmail.com>
2021-04-09 20:17:25 +0700

    Final

---
+++ myApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name1 = { "Mary", "John", "Peter", "Ann", "Barbara"};
+            List<string> list1 = new List<string>(name1);
+            list1 = list1.OrderBy(i => Guid.NewGuid()).ToList();
+            for(int i = 0; i<list1.Count; i++)
+            {
+                try
+                {
+                    Console.WriteLine($"{list1[i]}, {list1[i+1]}");
+                }
+                catch {
+                    Console.WriteLine($"{list1[4]}, {list1[0]}");
+                }
+            }
+        }
+    }
+}
